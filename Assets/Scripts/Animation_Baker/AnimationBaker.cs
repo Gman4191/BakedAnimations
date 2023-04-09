379535e182eb64034d6e30c9de10a4f2dc4857f9
Gman4191 <39177051+Gman4191@users.noreply.github.com>
2023-04-09 02:53:48 -0500

    Added Animations and Model

--- Assets/Scripts/Animation_Baker/AnimationBaker.cs
+++ Assets/Scripts/Animation_Baker/AnimationBaker.cs
@@ -12,6 +12,7 @@ public class AnimationBaker : MonoBehaviour
 {
     public ComputeShader infoTexGen;
     public Shader playShader;
+    public AnimationClip[] clips;
 
     public struct VertInfo
     {
@@ -20,29 +21,43 @@ public class AnimationBaker : MonoBehaviour
         public Vector3 tangent;
     }
 
-    // Use this for initialization
-    IEnumerator Start()
+    private void Reset()
     {
+        var animation = GetComponent<Animation>();
         var animator = GetComponent<Animator>();
-        var clips = animator.runtimeAnimatorController.animationClips;
+
+        if (animation != null)
+        {
+            clips = new AnimationClip[animation.GetClipCount()];
+            var i = 0;
+            foreach (AnimationState state in animation)
+                clips[i++] = state.clip;
+        }
+        else if (animator != null)
+            clips = animator.runtimeAnimatorController.animationClips;
+    }
+
+    // Use this for initialization
+    [ContextMenu("bake texture")]
+    void Bake()
+    {
         var skin = GetComponentInChildren<SkinnedMeshRenderer>();
         var vCount = skin.sharedMesh.vertexCount;
         var texWidth = Mathf.NextPowerOfTwo(vCount);
         var mesh = new Mesh();
 
-        animator.speed = 0;
-        foreach (var c in clips)
+        foreach (var clip in clips)
         {
-            var frames = Mathf.NextPowerOfTwo((int)(c.length / 0.05f));
+            var frames = Mathf.NextPowerOfTwo((int)(clip.length / 0.05f));
+            var dt = clip.length / frames;
             var infoList = new List<VertInfo>();
 
             var pRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
-            pRt.name = string.Format("{0}.{1}.posTex", name, c.name);
+            pRt.name = string.Format("{0}.{1}.posTex", name, clip.name);
             var nRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
-            nRt.name = string.Format("{0}.{1}.normTex", name, c.name);
+            nRt.name = string.Format("{0}.{1}.normTex", name, clip.name);
             var tRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
-            tRt.name = string.Format("{0}.{1}.tangentTex", name, c.name);
-
+            tRt.name = string.Format("{0}.{1}.tanTex", name, clip.name);
             foreach (var rt in new[] { pRt, nRt, tRt })
             {
                 rt.enableRandomWrite = true;
@@ -51,21 +66,20 @@ public class AnimationBaker : MonoBehaviour
                 GL.Clear(true, true, Color.clear);
             }
 
-            animator.Play(c.name);
-            yield return 0;
-
             for (var i = 0; i < frames; i++)
             {
-                animator.Play(c.name, 0, (float)i / frames);
-                yield return 0;
+                clip.SampleAnimation(gameObject, dt * i);
                 skin.BakeMesh(mesh);
 
+                var verexArry = mesh.vertices;
+                var normalArry = mesh.normals;
+                var tangentArray = mesh.tangents;
                 infoList.AddRange(Enumerable.Range(0, vCount)
                     .Select(idx => new VertInfo()
                     {
-                        position = mesh.vertices[idx],
-                        normal = mesh.normals[idx],
-                        tangent = mesh.tangents[idx]
+                        position = verexArry[idx],
+                        normal = normalArry[idx],
+                        tangent = tangentArray[idx],
                     })
                 );
             }
@@ -107,22 +121,22 @@ public class AnimationBaker : MonoBehaviour
             mat.SetTexture("_MainTex", skin.sharedMaterial.mainTexture);
             mat.SetTexture("_PosTex", posTex);
             mat.SetTexture("_NmlTex", normTex);
-            mat.SetFloat("_Length", c.length);
-            if (c.isLooping)
+            mat.SetFloat("_Length", clip.length);
+            if (clip.wrapMode == WrapMode.Loop)
             {
                 mat.SetFloat("_Loop", 1f);
                 mat.EnableKeyword("ANIM_LOOP");
             }
 
-            var go = new GameObject(name + "." + c.name);
+            var go = new GameObject(name + "." + clip.name);
             go.AddComponent<MeshRenderer>().sharedMaterial = mat;
             go.AddComponent<MeshFilter>().sharedMesh = skin.sharedMesh;
 
             AssetDatabase.CreateAsset(posTex, Path.Combine(subFolderPath, pRt.name + ".asset"));
             AssetDatabase.CreateAsset(normTex, Path.Combine(subFolderPath, nRt.name + ".asset"));
             AssetDatabase.CreateAsset(tanTex, Path.Combine(subFolderPath, tRt.name + ".asset"));
-            AssetDatabase.CreateAsset(mat, Path.Combine(subFolderPath, string.Format("{0}.{1}.animTex.asset", name, c.name)));
-            PrefabUtility.CreatePrefab(Path.Combine(folderPath, go.name + ".prefab").Replace("\\", "/"), go);
+            AssetDatabase.CreateAsset(mat, Path.Combine(subFolderPath, string.Format("{0}.{1}.animTex.asset", name, clip.name)));
+            PrefabUtility.SaveAsPrefabAsset(go, Path.Combine(folderPath, go.name + ".prefab").Replace("\\", "/"));
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
 #endif
