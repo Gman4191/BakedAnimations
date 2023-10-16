c7cf6fd9d69230431a02836f6fbf6b0cd8715ff5
Gman4191 <39177051+Gman4191@users.noreply.github.com>
2023-10-16 00:28:43 -0500

    Removed instance initialization job

--- Assets/Scripts/AssignmentJobs.cs
+++ Assets/Scripts/AssignmentJobs.cs
@@ -1,32 +1,6 @@
 using Unity.Burst;
 using Unity.Collections;
-using Unity.Jobs;
 using UnityEngine.Jobs;
-
-public struct InitializeObjectInfos : IJobParallelFor
-{
-    public NativeArray<objectInfo> _objectInfos;
-    public NativeArray<float> _yOffsets;
-    public NativeArray<float> _animationLengths;
-    public NativeArray<float> _animationScales;
-
-    [BurstCompile]
-    void IJobParallelFor.Execute(int i)
-    {
-        Unity.Mathematics.Random random = new Unity.Mathematics.Random((uint)i + 1);
-        int randomIndex = random.NextInt(0, _animationLengths.Length);
-
-        objectInfo obj = new objectInfo
-        {
-            isLooping = 0,
-            currentAnimation = _yOffsets[randomIndex],
-            animationScale = _animationScales[randomIndex],
-            animationLength = _animationLengths[randomIndex]
-        };
-
-        _objectInfos[i] = obj;
-    }
-}
 
 [BurstCompile]
 public struct AssignTransforms : IJobParallelForTransform
